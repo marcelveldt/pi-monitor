60e99c929ee1747991f0e9e24e8d77f1911030e7
Marcel van der Veldt <m.vanderveldt@outlook.com>
2018-08-08 20:26:31 +0200

    more work to installer

--- install.sh
+++ install.sh
@@ -2,22 +2,58 @@
 set -e
 
 
-if [ -d "/mnt/dietpi_userdata" ]; then
-    ROOT_DIR="/mnt/dietpi_userdata"
+install_service () {
+cat > /etc/systemd/system/pi-monitor.service <<'EOF'
+[Unit]
+Description=GPIO Monitoring
+After=multi-user.target
+
+[Service]
+Type=idle
+ExecStart=python $s/main.py
+Restart=always
+RestartSec=3
+
+[Install]
+WantedBy=multi-user.target
+EOF
+chmod +x /etc/systemd/system/pi-monitor.service
+sudo systemctl daemon-reload
+sudo systemctl enable pi-monitor.service 
+
+sudo systemctl start pi-monitor.service 
+}
+
+if [ -d "/mnt/dietpi_userdata/" ]; then
+    ROOT_DIR="/mnt/dietpi_userdata/"
+    IS_DIETPI=1
 else
-    ROOT_DIR="~"
+    ROOT_DIR="~/"
+    IS_DIETPI=0
 fi
 
-INSTALL_DIR="$ROOT_DIR/pi-monitor"
-echo "Installing Pi Monitor to $INSTALL_DIR"
+INSTALL_DIR="$ROOT_DIRpi-monitor"
 
 
 if [ ! -d "$INSTALL_DIR" ]; then
-        apt-get install -y build-essential git
+        echo "Installing Pi Monitor to $INSTALL_DIR"
+        if [ $IS_DIETPI == "1 "]; then
+            dietpi-software install 16
+            dietpi-software install 17
+            dietpi-software install 130
+        else
+            apt-get install -y build-essential git python-pip
+        fi
         cd $ROOT_DIR
         git clone https://github.com/marcelveldt/pi-monitor
+        install_service $INSTALL_DIR
+        
 else
     # just update the existing install
+    sudo systemctl stop pi-monitor.service 
+    echo "Updating Pi Monitor in $INSTALL_DIR"
     cd $INSTALL_DIR
     git pull
+    sudo systemctl start pi-monitor.service 
 fi
+

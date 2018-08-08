c3f08a3a961f5c38d9c46fff9409fd5d3b17af6d
Marcel van der Veldt <m.vanderveldt@outlook.com>
2018-08-08 20:52:42 +0200

    add some more verbose logging and prompts

--- install.sh
+++ install.sh
@@ -5,12 +5,12 @@ set -e
 install_service () {
 cat > /etc/systemd/system/pi-monitor.service <<'EOF'
 [Unit]
-Description=GPIO Monitoring
+Description=Pi Monitor
 After=multi-user.target
 
 [Service]
 Type=idle
-ExecStart=python $s/main.py
+ExecStart=python $1/main.py
 Restart=always
 RestartSec=3
 
@@ -38,7 +38,12 @@ INSTALL_DIR="$ROOT_DIRpi-monitor"
 
 
 if [ ! -d "$INSTALL_DIR" ]; then
+        echo "#########################################################################################################"
+        echo " "
+        echo " "
         echo "Installing Pi Monitor to $INSTALL_DIR"
+        echo "Some required packages will be automatically installed, please be patient."
+        read -p "Press enter to continue..."
         if [ "$IS_DIETPI" == "1" ]; then
             /DietPi/dietpi/dietpi-software install 16
             /DietPi/dietpi/dietpi-software install 17
@@ -49,6 +54,16 @@ if [ ! -d "$INSTALL_DIR" ]; then
         cd $ROOT_DIR
         git clone https://github.com/marcelveldt/pi-monitor
         install_service $INSTALL_DIR
+        echo "#########################################################################################################"
+        echo " "
+        echo " "
+        echo "Install Complete! "
+        echo "Access the Pi Monitor on http://localhost or http://ip-of-this-pi"
+        echo "Please note: at first launch the application will check several dependencies and perform some checks"
+        echo "On slower systems it can take up to 10 minutes before the webservice is ready."
+        echo " "
+        echo " "
+        echo "#########################################################################################################"
         
 else
     # just update the existing install

7d0f81400a67e08d572ac71c9ea3aeffb18f2014
Marcel van der Veldt <m.vanderveldt@outlook.com>
2018-08-15 18:19:46 +0200

    use the new websocket api for roon

--- modules/roon.py
+++ modules/roon.py
@@ -5,7 +5,7 @@
 import os
 import time
 import threading
-from resources.lib.utils import PlayerMetaData, json, requests, PLATFORM, check_software, run_proc, subprocess, DEVNULL, PLAYING_STATE, VOLUME_CONTROL_DISABLED
+from resources.lib.utils import PlayerMetaData, json, requests, PLATFORM, check_software, run_proc, subprocess, DEVNULL, PLAYING_STATE, VOLUME_CONTROL_DISABLED, import_or_install
 
 
 LOOP_WAIT = 4
@@ -18,10 +18,6 @@ def setup(monitor):
         return False
     is_armv6 = "armv6" in PLATFORM
     enable_squeezelite = monitor.config.get("ROON_USE_SQUEEZELITE", is_armv6)
-    roon_proxy = monitor.config.get("ROON_PROXY", u"http://192.168.1.1:3006")
-    if not roon_proxy:
-        LOGGER.warning("Roon proxy address is empty")
-        return False
     player_name = monitor.config.get("ROON_PLAYER_NAME", u"%hostname%")
     if not player_name:
         LOGGER.warning("Roon player name is empty")
@@ -38,7 +34,7 @@ def setup(monitor):
     if enable_squeezelite and monitor.config.get("ENABLE_MODULE_SQUEEZELITE", False):
         LOGGER.debug("Squeezelite module is enabled. You can not use Roon in squeezelite mode at the same time!")
         return False
-    # TODO: check if service is actually installed
+    import_or_install("roon", "RoonApi", True, installpip="roonapi")
     return RoonPlayer(monitor, roon_proxy, player_name, enable_squeezelite)
 
 
@@ -48,6 +44,7 @@ class RoonPlayer(threading.Thread):
     _output_id = None
     _last_state = None
     _squeezelite_proc = None
+    _roonapi = None
 
     def __init__(self, monitor, proxy_address, player_name, enable_squeezelite):
         threading.Thread.__init__(self)
@@ -56,6 +53,17 @@ class RoonPlayer(threading.Thread):
         self.monitor = monitor
         self.enable_squeezelite = enable_squeezelite
         self.monitor.states["roon"] = PlayerMetaData("Roon")
+        appinfo = {
+            "extension_id": "pi_monitor",
+            "display_name": "Pi Monitor",
+            "display_version": "1.0.0",
+            "publisher": "marcelveldt",
+            "email": "marcelveldt@users.noreply.github.com"
+        }
+        token = monitor.config.get("ROON_AUTH_TOKEN")
+        self._roonapi = RoonApi(token)
+        monitor.config["ROON_AUTH_TOKEN"] = self._roonapi.token
+        self._roonapi.register_state_callback(self._roon_state_callback, event_filter="zones_changed", id_filter=self.player_name)
         
 
     @property   
@@ -71,24 +79,20 @@ class RoonPlayer(threading.Thread):
         self._exit.set()
         if self._squeezelite_proc:
             self._squeezelite_proc.terminate()
+        if self._roonapi:
+            self._roonapi.stop()
         threading.Thread.join(self, 10)
 
     def command(self, cmd, cmd_data=None):
         ''' send command to roon output/zone'''
         if cmd == "volume_up":
-            return self._api_execute(
-                    "change_volume_relative", {"volume":2, "output": self.output_id})
+            return self._roonapi.change_volume(self.output_id, 2, "relative")
         elif cmd == "volume_down":
-            return self._api_execute(
-                    "change_volume_relative", {"volume":-2, "output": self.output_id})
+            return self._roonapi.change_volume(self.output_id, -2, "relative")
         elif cmd == "volume_set":
             return self._set_volume(cmd_data)
         else:
-            params = {
-                "zone": self.output_id,
-                "control": cmd
-            }
-            return self._api_execute("control", params=params)
+            return self._roonapi.playback_control(self.output_id, cmd)
 
     def run(self):
         if self.enable_squeezelite:
@@ -105,50 +109,43 @@ class RoonPlayer(threading.Thread):
 
         # some players need to be unmuted when freshly started
         if self.output_id:
-            self._api_execute("mute", {"how":"unmute", "output": self.output_id})
-        # loop: poll for player changes
-        # TODO: use pure websockets implementation to access Roon
+            self._roonapi.mute(self.output_id, False)
+
+        # mainloop: just keep the thread alive
         while not self._exit.isSet():
-            cur_state = self._get_state()
-            if cur_state != self._last_state:
-                self._last_state = cur_state
-                self._update_metadata()
-            if cur_state == PLAYING_STATE:
-                self._update_metadata()
-                LOOP_WAIT = 0.5
-            else:
-                LOOP_WAIT = 2
-            self._exit.wait(LOOP_WAIT)
+            self._exit.wait(1200)
 
 
     #### PRIVATE CLASS METHODS #######
 
+    def _roon_state_callback(self, event, changed_items):
+        '''will be called when roon reports events for our player'''
+        self._update_metadata()
+
     def _get_output_id(self):
         ''' get output_id for this player'''
-        output_id = self._api_request("output_id_by_name", {"name": self.player_name})
-        if output_id:
+        output_id = ""
+        output = self._roonapi.output_by_name(self.player_name)
+        if output:
+            output_id = output["output_id"]
             LOGGER.debug("detected output id: %s" % output_id)
         else:
             LOGGER.debug("unable to detect Roon output ID, skip for now...")
-            output_id = None
         return output_id
-
-    def _get_state(self):
-        return self._api_request("zone_state", {"output": self.output_id})
 
     def _set_volume(self, volume_level):
         ''' set volume level '''
-        output_details = self._api_request("output", {"output": self.output_id})
+        output_details = self._roonapi.outputs[self.output_id]
         if output_details and output_details.get("volume"):
             if output_details["volume"]["type"] == "db":
                 volume_level = int((float(volume_level) / 100) * 80) - 80
-            return self._api_execute("change_volume", {"volume":volume_level, "output": self.output_id})
+            return self._roonapi.change_volume(self.output_id, volume_level)
         return False
 
     def _get_volume(self):
         ''' get current volume level of player'''
         vol_level = 0
-        output_details = self._api_request("output", {"output": self.output_id})
+        output_details = self._roonapi.outputs[self.output_id]
         if output_details and output_details.get("volume"):
             if output_details["volume"]["type"] == "db":
                 vol_level = int(float(output_details['volume']['value']) / 80 * 100 + 100)
@@ -157,7 +154,7 @@ class RoonPlayer(threading.Thread):
         return vol_level
 
     def _update_metadata(self):
-        zone_details = self._api_request("zone_by_output_id", {"output": self.output_id})
+        zone_details = self._roonapi.zone_by_output_name(self.player_name)
         self.monitor.states["roon"]["state"] = zone_details.get("state")
         self.monitor.states["roon"]["volume_level"] = self._get_volume()
         if zone_details and zone_details.get("now_playing"):
@@ -167,7 +164,7 @@ class RoonPlayer(threading.Thread):
             self.monitor.states["roon"]["title"] = zone_details["three_line"]["line1"]
             self.monitor.states["roon"]["duration"] = zone_details["length"]
             if zone_details["image_key"]:
-                url = '%s/image?image_key=%s&width=500&height=500&scale=fit' % (self.proxy_address, zone_details["image_key"])
+                url = self._roonapi.get_image(zone_details["image_key"])
                 self.monitor.states["roon"]["cover_url"] = url
             else:
                 self.monitor.states["roon"]["cover_url"] = ""
@@ -177,34 +174,3 @@ class RoonPlayer(threading.Thread):
             self.monitor.states["roon"]["title"] = ""
             self.monitor.states["roon"]["duration"] = ""
             self.monitor.states["roon"]["cover_url"] = ""
-
-    def _api_request(self, endpoint, params=None):
-        '''get info from json api'''
-        result = {}
-        url = "{}/{}".format(self.proxy_address, endpoint)
-        params = params if params else {}
-        try:
-            response = requests.get(url, params=params, timeout=10)
-            if response and response.content and response.status_code == 200:
-                if "{" in response.content:
-                    result = json.loads(response.content.decode('utf-8', 'replace'))
-                else:
-                    result = response.content.decode('utf-8')
-            else:
-                LOGGER.debug("Invalid or empty reponse from server - endpoint: %s - server response: %s - %s" %
-                        (endpoint, response.status_code, response.content))
-        except Exception as exc:
-            LOGGER.error(exc)
-            result = None
-        return result
-
-    def _api_execute(self, endpoint, params=None):
-        '''execute command on json api without waiting for result'''
-        url = "{}/{}".format(self.proxy_address, endpoint)
-        params = params if params else {}
-        try:
-            requests.get(url, params=params, timeout=0.5)
-            return True
-        except Exception as exc:
-            LOGGER.debug(exc)
-            return False

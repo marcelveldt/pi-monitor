0377f7892a44f977cf26e29a8184925a543b8b65
Marcel van der Veldt <m.vanderveldt@outlook.com>
2018-08-19 02:33:00 +0200

    do not update pip packages

--- run.sh
+++ run.sh
@@ -7,7 +7,6 @@ then
     echo "Auto updating to latest version..."
     git fetch --all
     git reset --hard origin/master
-    pip list --outdated --format=freeze | grep -v '^\-e' | cut -d = -f 1  | xargs -n1 pip install -U
 else
     echo "Auto update is disabled"
 fi
